8bc2ffb19c8c7827304492352eddd23decec3c29
casaras1 <164526009+casaras1@users.noreply.github.com>
2025-03-11 14:48:32 -0400

    Update module-info.java

--- tinkar-starter-data/src/main/resources/module-info.java
+++ tinkar-starter-data/src/main/resources/module-info.java
@@ -1,9 +1,6 @@
-open module tinkar.starter.data {	
-	requires dev.ikm.tinkar.reasoner.service;
+open module dev.ikm.tinkar {	
 	requires dev.ikm.tinkar.provider.ephemeral;
 	requires dev.ikm.tinkar.reasoner.elkowl;
-	requires dev.ikm.tinkar.reasoner.elksnomed;
-	requires dev.ikm.tinkar.reasoner.hybrid;
 	requires org.junit.jupiter.api;
     requires dev.ikm.tinkar.common;
     requires dev.ikm.tinkar.composer;
@@ -13,6 +10,8 @@ open module tinkar.starter.data {
     requires dev.ikm.tinkar.provider.entity;
     requires dev.ikm.jpms.eclipse.collections.api;
     requires org.slf4j;
-	//requires maven.plugin.api; 
-	//requires maven.plugin.annotations;
+	requires dev.ikm.tinkar.reasoner.service;
+	
+	// requires maven.plugin.api; 
+	// requires maven.plugin.annotations;
 }
