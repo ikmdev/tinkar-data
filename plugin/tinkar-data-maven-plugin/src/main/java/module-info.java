30e9d7bbd646896e46cf1991201938e0a9034df8
casaras1 <164526009+casaras1@users.noreply.github.com>
2025-03-17 12:39:30 -0400

    Update module-info.java

--- plugin/tinkar-data-maven-plugin/src/main/java/module-info.java
+++ plugin/tinkar-data-maven-plugin/src/main/java/module-info.java
@@ -4,14 +4,14 @@ open module dev.ikm.tinkar {
 	requires dev.ikm.tinkar.reasoner.elkowl;
 	requires dev.ikm.tinkar.reasoner.elksnomed;
 	requires dev.ikm.tinkar.reasoner.hybrid;
-    requires dev.ikm.tinkar.common;
-    requires dev.ikm.tinkar.composer;
-    requires dev.ikm.tinkar.entity;
-    requires dev.ikm.tinkar.provider.executor;
-    requires dev.ikm.tinkar.provider.spinedarray;
-    requires dev.ikm.tinkar.provider.entity;
-    requires dev.ikm.jpms.eclipse.collections.api;
-    requires org.slf4j;
+    	requires dev.ikm.tinkar.common;
+    	requires dev.ikm.tinkar.composer;
+    	requires dev.ikm.tinkar.entity;
+    	requires dev.ikm.tinkar.provider.executor;
+    	requires dev.ikm.tinkar.provider.spinedarray;
+    	requires dev.ikm.tinkar.provider.entity;
+    	requires dev.ikm.jpms.eclipse.collections.api;
+    	requires org.slf4j;
 	requires maven.plugin.api; 
 	requires maven.plugin.annotations;
 }
